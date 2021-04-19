c0dcbaeab8398d141136a8a671809139f7c4aa6f
daniyal_741 <daniyalanwaar741@gmail.com>
2021-04-19 13:05:46 +0500

    Carousel Added

---
+++ js/carousel.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:1,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        768:{
+            items:3,
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
